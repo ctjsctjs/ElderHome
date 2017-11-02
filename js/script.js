c3b2263f5a1f18b7e277371d637e3a3a6c92e3ce
ctjsctjs <ctjsctjs@gmail.com>
2017-11-03 00:24:08 +0800

    0311

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  console.log("OK");
+
+  $('#nav-hamburger').click(function(){
+    $(".nav-center").slideToggle(100, "linear");
+  });
+
+});
+
+function increaseValue() {
+  var value = parseInt(document.getElementById('number').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  document.getElementById('number').value = value;
+};
+
+function decreaseValue() {
+  var value = parseInt(document.getElementById('number').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value < 1 ? value = 1 : '';
+  value--;
+  document.getElementById('number').value = value;
+};
