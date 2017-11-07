4a9ba44dfbcc88a8f57b3d4e3d4991cd6c2656c9
ctjsctjs <ctjsctjs@gmail.com>
2017-11-07 17:15:53 +0800

    07/11 - 17:15
    Add notification icon and dropdown for navigation
    Fix register page
    Add staff schedule

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 $(document).ready(function () {
   console.log("OK");
 
+  /*Mobile nav dropdown trigger*/
   $('#nav-hamburger').click(function(){
     $(".nav-mobile").slideToggle(100, "linear");
   });
+
+  /*Profile menu dropdown trigger*/
+  $('.profile-dropdown').click(function(){
+    $(".dropdown").fadeToggle(100, "linear");
+  });
 });
 
+/*Map level increase button*/
 function increaseValue() {
   var value = parseInt(document.getElementById('number').value, 10);
   value = isNaN(value) ? 0 : value;
@@ -13,6 +20,7 @@ function increaseValue() {
   document.getElementById('number').value = value;
 };
 
+/*Map level decrease button*/
 function decreaseValue() {
   var value = parseInt(document.getElementById('number').value, 10);
   value = isNaN(value) ? 0 : value;
